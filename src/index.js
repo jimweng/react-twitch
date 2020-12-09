9304ab496d77a665112a3b86242e0aeef55f2110
jimweng <jimweng01@hotmail.com>
2020-12-09 10:27:06 +0800

    create multiple actions

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
 import reducers from './reducers';
+import redeuxThunk from 'redux-thunk';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducers,
   composeEnhancer(
-  applyMiddleware()
+  applyMiddleware(redeuxThunk)
 ));
 
 ReactDOM.render(
